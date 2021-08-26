2155403917876e63a6f575b3ecff0cb84e266367
Steve Song <sshtel@gmail.com>
2021-08-26 11:48:41 +0900

    busybox-custom printout request info

--- busybox-custom/server/app.js
+++ busybox-custom/server/app.js
@@ -3,6 +3,10 @@ const app = express()
 const port = process.env.PORT || 8080
 
 app.get('/', (req, res) => {
+  console.log('received request GET /')
+  console.log(req.rawHeaders)
+  const ip = req.headers['cf-connecting-ip'] || req.headers['x-forwarded-for'] || req.socket.remoteAddress
+  console.log(ip)
   res.send('Hello World from BusyBox Custom')
 })
 
